4f6103721f4e3c9779e3dd59f574253a48134f3c
Samuel Mapusao Borja <120239552+smb23@users.noreply.github.com>
2023-02-01 21:57:50 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res)=>{
+    res.send("welcome")
+},
+(req, res)=>{
+
+});
+
+app.get('/home',(req,res)=>{
+    res.render('home')
+});
+
+app.get('/contact-us',(req,res)=>{
+    res.render('contact-us')
+});
+
+app.get('/about-us',(req,res)=>{
+    res.render('about')
+});
+
+app.listen(3000);
+console.log("running in port 3000");
+
+
+
